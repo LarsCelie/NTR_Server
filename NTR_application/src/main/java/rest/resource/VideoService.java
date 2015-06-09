262d5b8599bf7c65eacfd811edcc2b6c622d9400
Milamber <ff-denke@hotmail.com>
2015-06-10 00:34:57 +0200

    videoservice

---
+++ NTR_application/src/main/java/rest/resource/VideoService.java
@@ -0,0 +1,96 @@
+package main.java.rest.resource;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
+
+import main.java.services.MediaStreamer;
+
+import java.io.*;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
+import java.nio.channels.WritableByteChannel;
+import java.util.Date;
+
+
+@Path("/listen/{id}")
+public class VideoService {
+
+    final int chunk_size = 1024 * 1024; // 1MB chunks
+    private final File audio;
+
+    public VideoService(@PathParam("id") String id) {
+        // serve media from file system
+        String MEDIA_FILE = "c:/NTR/Upload/video/"+id+".mp4";
+        audio = new File(MEDIA_FILE);
+    }
+
+    @HEAD
+    @Produces("video/mp4")
+    public Response header() {
+        return Response.ok().status(206).header(HttpHeaders.CONTENT_LENGTH, audio.length()).build();
+    }
+
+    @GET
+    @Produces("video/mp4")
+    public Response streamAudio(@HeaderParam("Range") String range) throws Exception {
+        return buildStream(audio, range);
+    }
+
+    /**
+     * Adapted from http://stackoverflow.com/questions/12768812/video-streaming-to-ipad-does-not-work-with-tapestry5/12829541#12829541
+     *
+     * @param asset Media file
+     * @param range range header
+     * @return Streaming output
+     * @throws Exception IOException if an error occurs in streaming.
+     */
+    private Response buildStream(final File asset, final String range) throws Exception {
+        // range not requested : Firefox, Opera, IE do not send range headers
+        if (range == null) {
+            StreamingOutput streamer = new StreamingOutput() {
+                @Override
+                public void write(final OutputStream output) throws IOException, WebApplicationException {
+
+                    final FileChannel inputChannel = new FileInputStream(asset).getChannel();
+                    final WritableByteChannel outputChannel = Channels.newChannel(output);
+                    try {
+                        inputChannel.transferTo(0, inputChannel.size(), outputChannel);
+                    } finally {
+                        // closing the channels
+                        inputChannel.close();
+                        outputChannel.close();
+                    }
+                }
+            };
+            return Response.ok(streamer).status(200).header(HttpHeaders.CONTENT_LENGTH, asset.length()).build();
+        }
+
+        String[] ranges = range.split("=")[1].split("-");
+        final int from = Integer.parseInt(ranges[0]);
+        /**
+         * Chunk media if the range upper bound is unspecified. Chrome sends "bytes=0-"
+         */
+        int to = chunk_size + from;
+        if (to >= asset.length()) {
+            to = (int) (asset.length() - 1);
+        }
+        if (ranges.length == 2) {
+            to = Integer.parseInt(ranges[1]);
+        }
+
+        final String responseRange = String.format("bytes %d-%d/%d", from, to, asset.length());
+        final RandomAccessFile raf = new RandomAccessFile(asset, "r");
+        raf.seek(from);
+
+        final int len = to - from + 1;
+        final MediaStreamer streamer = new MediaStreamer(len, raf);
+        Response.ResponseBuilder res = Response.ok(streamer).status(206)
+                .header("Accept-Ranges", "bytes")
+                .header("Content-Range", responseRange)
+                .header(HttpHeaders.CONTENT_LENGTH, streamer.getLenth())
+                .header(HttpHeaders.LAST_MODIFIED, new Date(asset.lastModified()));
+        return res.build();
+    }
+}
