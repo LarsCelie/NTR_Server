7be9f180f620859388e2e4e02d5245babe904a7b
Lars <LarsCelie@gmail.com>
2015-06-22 13:35:44 +0200

    small edit for constructor Utility class

--- NTR_application/src/main/java/util/Utility.java
+++ NTR_application/src/main/java/util/Utility.java
@@ -3,43 +3,40 @@ package main.java.util;
 import java.util.HashSet;
 
 public final class Utility {
-	
+
 	private HashSet<String> video;
 	private HashSet<String> audio;
 	private HashSet<String> image;
-	
+
 	private static Utility util;
 
-	
-	private Utility(){
-		util = this;
+	private Utility() {
 		video = new HashSet<String>();
 		video.add("mp4");
-		
+
 		audio = new HashSet<String>();
 		audio.add("mp3");
-		
+
 		image = new HashSet<String>();
 		image.add("png");
 	}
-	
-	public static Utility getUtility(){
-		if (util == null){
-			return new Utility();
- 		} else {
- 			return util;
- 		}
+
+	public static Utility getUtility() {
+		if (util == null) {
+			util = new Utility();
+		}
+		return util;
 	}
-	
-	public boolean isVideo(String fileExtension){
+
+	public boolean isVideo(String fileExtension) {
 		return video.contains(fileExtension);
 	}
-	
-	public boolean isAudio(String fileExtension){
+
+	public boolean isAudio(String fileExtension) {
 		return audio.contains(fileExtension);
 	}
-	
-	public boolean isImage(String fileExtension){
+
+	public boolean isImage(String fileExtension) {
 		return image.contains(fileExtension);
 	}
 }
