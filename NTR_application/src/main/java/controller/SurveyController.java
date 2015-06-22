849c4f7a07aecd3da4f085a4fc2dc315afb712f4
Lars <LarsCelie@gmail.com>
2015-06-22 13:33:15 +0200

    Added util class and save files to directory.

--- NTR_application/src/main/java/controller/SurveyController.java
+++ NTR_application/src/main/java/controller/SurveyController.java
@@ -1,6 +1,11 @@
 package main.java.controller;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -10,10 +15,11 @@ import java.util.List;
 import main.java.dao.QuestionDao;
 import main.java.dao.ResearchDao;
 import main.java.dao.SurveyDao;
+import main.java.domain.Attachment;
 import main.java.domain.Option;
 import main.java.domain.Question;
 import main.java.domain.Survey;
-import oracle.net.aso.q;
+import main.java.util.Utility;
 
 import org.glassfish.jersey.media.multipart.BodyPart;
 import org.glassfish.jersey.media.multipart.MultiPart;
@@ -30,93 +36,127 @@ public class SurveyController {
 	public List<Survey> getSurveys() {
 		return new SurveyDao().readAll();
 	}
-	
+
 	public boolean postSurvey(final MultiPart multiPart) {
-		/*	0: researchid
-		 *  1: name
-		 *  2: begin date
-		 *  3: end date
-		 *  
-		 *  4+ = questions
+		/*
+		 * 0: researchid 1: name 2: begin date 3: end date
+		 * 
+		 * 4+ = questions
 		 */
 		int researchid = multiPart.getBodyParts().get(0).getEntityAs(Integer.class);
 		String name = multiPart.getBodyParts().get(1).getEntityAs(String.class);
 		String begindate = multiPart.getBodyParts().get(2).getEntityAs(String.class);
 		String enddate = multiPart.getBodyParts().get(3).getEntityAs(String.class);
-		
+
 		SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
 		formatter.setLenient(true);
 		Date begin = null;
 		Date end = null;
-		try{
+		try {
 			begin = formatter.parse(begindate);
 			end = formatter.parse(enddate);
-		} catch(ParseException e){
-			//invalid date
+		} catch (ParseException e) {
+			// invalid date
 			return false;
 		}
-		
+
 		Survey survey = new Survey();
 		survey.setBeginDate(begin);
 		survey.setEndDate(end);
 		survey.setName(name);
 		survey.setResearch(new ResearchDao().load(researchid));
-		
-		
+		new SurveyDao().create(survey);
+		//create Survey so the ID can be accessed
+
 		List<BodyPart> parts = multiPart.getBodyParts();
 		Question q = null;
 		int sequence = 0;
 		List<Option> options = new ArrayList<Option>();
 		List<Question> questions = new ArrayList<Question>();
-		for (int i = 4; i < parts.size(); i++){
+		for (int i = 4; i < parts.size(); i++) {
 			BodyPart part = parts.get(i);
 			String contentName = part.getContentDisposition().getParameters().get("name");
-			if (contentName.equals("questionIllustrationFile")){
-				
+			if (contentName.equals("questionIllustrationFile")) {
+
 				sequence++;
 				q = new Question();
 				questions.add(q);
 				q.setSequence(sequence);
 				q.setSurvey(survey);
-				
-				String fileName = part.getContentDisposition().getFileName();	
-				if (!fileName.isEmpty()){
-					System.out.println(fileName);
+
+				String fileName = part.getContentDisposition().getFileName();
+				if (!fileName.isEmpty()) {
+					String type = null;
+					String[] split = fileName.split(".");
+					String extension = split[(split.length-1)];
+					Utility util = Utility.getUtility();
+					if (util.isAudio(extension)){
+						type = "audio";
+					} else if (util.isVideo(extension)){
+						type = "video";
+					} else if (util.isImage(extension)){
+						type = "image";
+					}
+					if (type != null){
+						InputStream fileStream = part.getEntityAs(FileInputStream.class);
+						String newFileName = researchid + "_" + survey.getId() + "_" + sequence;
+						boolean save = saveAsFile(fileStream, type, newFileName);
+						if (save){
+							Attachment a = new Attachment();
+							a.setLocation(newFileName);
+							a.setQuestion(q);
+							a.setType(type);
+						}
+					} else {
+						System.out.println("Type unsupported");
+					}
 				} else {
-					System.out.println("No file!");
+					System.out.println("No file attached, so not adding attachment to question");
 				}
 			} else if (contentName.equals("questionType")) {
 				String type = part.getEntityAs(String.class);
 				q.setType(type);
-			} else if (contentName.equals("questionValue")){
+			} else if (contentName.equals("questionValue")) {
 				String value = part.getEntityAs(String.class);
 				q.setDescription(value);
-			} else if (contentName.equals("questionOption")){
+			} else if (contentName.equals("questionOption")) {
 				String option = part.getEntityAs(String.class);
 				Option o = new Option();
 				o.setQuestion(q);
 				o.setValue(option);
 				options.add(o);
 			}
-			//questionIllustrationFile
-			//questionType
-			//questionValue
-			//questionOption
-//			String x = part.getContentDisposition().toString();
-//			System.out.println(x);
 		}
-		
-		new SurveyDao().create(survey);
+		//save the questions
 		QuestionDao qd = new QuestionDao();
-		for (Question quest : questions){
+		for (Question quest : questions) {
 			qd.create(quest);
 		}
 		return true;
 	}
-	
-	private boolean fileHandler(File file){
-		
-		return false;
+
+	private boolean saveAsFile(InputStream inputStream, String type, String fileName) {
+		try {
+			OutputStream outStream = new FileOutputStream("C:/NTR/UPLOAD/"+type.toUpperCase() + "/" + fileName); //Save the file
+			byte[] buffer = new byte[4096];
+			int bytesRead = -1;
+			while ((bytesRead = inputStream.read(buffer)) != -1) {
+				outStream.write(buffer, 0, bytesRead);
+			}
+			outStream.flush();
+			outStream.close();
+			
+			return true;
+		} catch (IOException ioex) {
+			return false;
+		} finally {
+			try {
+				inputStream.close();
+			} catch (IOException e) {
+				System.out.println("Inputstream not closed");
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
